4fb7a9a5e796b91199b4aecd955cd3e184d9dfa0
tflave <franknguyen007@gmail.com>
2014-07-28 15:35:28 +0700

    Load map
    Load google maps to the web

--- TrafficMap/urls.py
+++ TrafficMap/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'TrafficMap.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('DrawMap.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
